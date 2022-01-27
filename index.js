ddb3001e7ba98fe198cf89f8810c33e58a2cb642
Ashley N Dennis <a.n.d@Ashleys-MacBook-Air.local>
2022-01-27 12:07:16 -0500

    connected to firestore and added restaurant

--- index.js
+++ index.js
@@ -5,10 +5,25 @@ const restaurants = require('./restaurants.json');
 const {initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
-// connect to Firestore
-
+//import credentials
 const credentials = require('./credentials.json');
+
+//connect to firebasae services
+initializeApp({
+    credential: cert(credentials)
+});
+
+// connect to Firestore
+const db = getFirestore();
 
 // create a collection called "restaurants"
 
+
 // add each restaurant
+db.collection('restaurants').add(restaurants[1])
+.then(doc => {
+    console.log('Added restaurant', doc.id); // id auto-generated 
+})
+.catch(err =>{
+    console.error(err);
+});
